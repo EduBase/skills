4b857df2d8c613cdb9815b8c0fa3b66d0f111dc8
Aron Hives <icebird93@gmail.com>
2026-03-27 08:16:33 +0100

    Add pre-commit configuration

---
+++ .markdown-style.rb
@@ -0,0 +1,16 @@
+# see: https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md
+all
+rule 'MD007', :indent => 4
+rule 'MD009', :br_spaces => 2
+exclude_rule 'MD007'
+exclude_rule 'MD013'
+exclude_rule 'MD022'
+exclude_rule 'MD024'
+exclude_rule 'MD026'
+exclude_rule 'MD029'
+exclude_rule 'MD031'
+exclude_rule 'MD032'
+exclude_rule 'MD033'
+exclude_rule 'MD034'
+exclude_rule 'MD040'
+exclude_rule 'MD041'
